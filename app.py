d51459c8bd855199199b32c096c807f4121a8742
kendiva <kendiva17@gmail.com>
2025-05-17 11:03:45 +0700

    init

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return render_template("withmodals.html")
+    return render_template("index.html")
+
+
+@app.route("/map")
+def map():
+    return render_template("map.html")
+
+
+@app.route("/room")
+def room():
+    return render_template("room.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
